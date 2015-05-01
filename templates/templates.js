cd8b87ab8d22e339584807df1b3e1e39ae7c34e4
massicattaneo <massi.cattaneo@alice.it>
2015-05-01 22:46:08 +0200

    indovina chi

--- templates/templates.js
+++ templates/templates.js
@@ -8,7 +8,7 @@ templates["board"] = "<div>\n" +
    "</div>\n" +
    "";
 
-templates["card"] = "<li onclick=\"this.getElementsByTagName('div')[0].className = 'selected'\">\n" +
+templates["card"] = "<li onclick=\"setSelected(this)\">\n" +
    "    <img src=\"images/1.png\" class=\"photo\" />\n" +
    "    <span class=\"label\" data-item=\"label\">LABEL</span>\n" +
    "    <div data-item=\"selected\">X</div>\n" +

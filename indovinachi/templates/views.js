05c07837ded8709473b4dce5712bcdb0adf72c5a
massicattaneo <massi.cattaneo@alice.it>
2015-06-01 18:36:45 +0200

    Web Examples

---
+++ indovinachi/templates/views.js
@@ -0,0 +1,29 @@
+var views = {};
+
+views["board"] = "<div>\n" +
+   "    <div>\n" +
+   "        <ul>\n" +
+   "            <li data-collection=\"card:cards\"></li>\n" +
+   "        </ul>\n" +
+   "        <div style=\"clear:both;\"></div>\n" +
+   "    </div>\n" +
+   "</div>\n" +
+   "";
+
+views["card"] = "<li>\n" +
+   "    <div data-listener=\"flip:click\">\n" +
+   "        <div data-item=\"faces\">\n" +
+   "            <figure class=\"front\">\n" +
+   "                <div style=\"display: block; position: relative;\">\n" +
+   "                    <img data-bind=\"src:imagepath\" class=\"photo\" />\n" +
+   "                    <span data-bind=\"value:fullname\"></span>\n" +
+   "                </div>\n" +
+   "            </figure>\n" +
+   "            <figure class=\"back\">\n" +
+   "                <div>\n" +
+   "                    ?\n" +
+   "                </div>\n" +
+   "            </figure>\n" +
+   "        </div>\n" +
+   "    </div>\n" +
+   "</li>";
